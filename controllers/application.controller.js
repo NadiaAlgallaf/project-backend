e9cd34354a7064cf46261c1abc0633e7a127eaf8
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-08-11 10:24:26 +0300

    create function to set interview date

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -179,3 +179,22 @@ export const deleteApplication = async (req, res) => {
     })
   }
 }
+
+//Update Interview Date
+export const updateInterviewDate = async (req, res) => {
+  try {
+    const application = await Application.findById(req.params.id)
+
+    if (!application) {
+      return res.status(404).json({ message: 'Application not found' })
+    }
+
+    application.interviewDate = req.body.interviewDate
+
+    await application.save()
+
+    res.status(200).json(application)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
